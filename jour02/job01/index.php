5d7c4a5ae9a8180517eb20fe4da59e3794803493
Marceau MAUBERT <marceau.maubert@laplateforme.io>
2020-03-10 10:30:27 +0100

    Jour 02 - Job 01

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script src="script.js"></script>
+</head>
+<body>
+	<article id="citation">
+		La vie a beaucoup plus d'imagination que nous
+	</article>
+	<button class="button" onclick="citation()">console.log</button>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,4 @@
+function citation() {
+	let citation = document.getElementById("citation");
+	console.log(citation.innerText.trim());
+}
