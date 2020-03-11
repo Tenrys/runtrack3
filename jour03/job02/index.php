3d3507644e8c4b38afa1daa0ee402eaf060614a1
Marceau MAUBERT <marceau.maubert@laplateforme.io>
2020-03-11 11:20:52 +0100

    Jour 03 - Job 02

---
+++ jour03/job02/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="script.js"></script>
+	<style>
+		div {
+			border: 4px solid black;
+			padding: 1em;
+		}
+	</style>
+</head>
+<body>
+	<button id="button">Mélanger</button>
+	<div id="melangees"></div>
+	<div id="rangees"></div>
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,38 @@
+let arcEnCiel = [];
+
+$(document).ready(() => {
+	for (let i = 0; i < 6; i++) {
+		const part = $("<img src='arc" + (i + 1) + ".png'/>");
+		part.click(function() {
+			if (
+				$(this)
+					.parent()
+					.attr("id") !== "rangees"
+			) {
+				$(this).appendTo($("#rangees"));
+			}
+		});
+		arcEnCiel.push(part);
+	}
+
+	function shuffle() {
+		arcEnCiel
+			.sort(function() {
+				return 0.5 - Math.random();
+			})
+			.forEach(el => $("#melangees").append(el));
+	}
+	shuffle();
+
+	$("#button").click(() => {
+		$("img").map((_, img) => {
+			if (
+				$(img)
+					.parent()
+					.attr("id") !== "melangees"
+			) {
+				shuffle();
+			}
+		});
+	});
+});
