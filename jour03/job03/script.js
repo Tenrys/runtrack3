5f8e79bb7ed3d2f74f1f3445c65faddcd897da8c
marceau-maubert <marceau.maubert2@gmail.com>
2020-03-11 18:47:04 +0100

    Condition victoire jour 03 job 03

--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -4,24 +4,54 @@ $(document).ready(() => {
 	const rand = Math.ceil(Math.random() * 9) - 1;
 	for (let i = 0; i < 9; i++) {
 		let part;
-		if (i == rand) {
+		if (i === rand) {
 			part = $("<div style='width: 184px; height: 184px;'/>");
 		} else {
-			part = $("<img src='" + (i + 1) + ".png'/>");
+			part = $(`<img src='${i}.png'/>`);
 		}
 		LaPlateforme.push(part);
 	}
-	let empty = LaPlateforme[rand];
-	LaPlateforme.sort(() => {
-		return 0.5 - Math.random();
-	});
-	empty = LaPlateforme[LaPlateforme.indexOf(empty)];
+	const empty = LaPlateforme[rand];
+
+	function shuffle() {
+		LaPlateforme.sort(() => {
+			return 0.5 - Math.random();
+		});
+	}
+	shuffle();
+
+	function checkWin() {
+		for (let i = 0; i < $("td").length; i++) {
+			const el = $("td")[i];
+			const child = $(el).children()[0];
+			if (
+				!["div", "img"].includes(child.tagName.toLowerCase()) ||
+				(child.tagName.toLowerCase() === "img" && $(child).attr("src") !== `${i}.png`)
+			) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	function refreshTable() {
 		$("td").empty();
-		$("td").map((i, td) => {
-			$(td).append(LaPlateforme[i]);
-		});
+		for (let i = 0; i < $("td").length; i++) {
+			const el = $("td")[i];
+			$(el).append(LaPlateforme[i]);
+		}
+
+		if (checkWin()) {
+			const winMsg = $("<h1 id='result' class='win'>Vous avez gagné</h1>");
+			const retry = $("<button>Recommencer</button>");
+			retry.click(() => {
+				$(winMsg).remove();
+				$(retry).remove();
+				shuffle();
+				refreshTable();
+			});
+			$("body").append(winMsg, retry);
+		}
 	}
 	refreshTable();
 
@@ -32,14 +62,15 @@ $(document).ready(() => {
 		arrowright: -1
 	};
 	$(document).keydown(ev => {
-		let key = ev.key.toLowerCase();
+		if ($("#result").length > 0) return;
 
+		const key = ev.key.toLowerCase();
 		const emptyPos = LaPlateforme.indexOf(empty);
 		const el = LaPlateforme[emptyPos + adjacent[key]];
 		const newPos = LaPlateforme.indexOf(el);
 
 		if (el) {
-			if ((key == "arrowleft" && newPos % 3 === 0) || (key == "arrowright" && newPos % 3 === 2)) return;
+			if ((key === "arrowleft" && newPos % 3 === 0) || (key === "arrowright" && newPos % 3 === 2)) return;
 
 			LaPlateforme[emptyPos] = el;
 			LaPlateforme[newPos] = empty;
